b139e5afcf32103afd1f56bff7f45c7b91bf5368
unknown <lattabun@gmail.com>
2023-04-15 19:51:07 -0400

    README.md now gets added to when questions are answered

---
+++ Develop/README.md
@@ -0,0 +1,11 @@
+# title
+## Description
+descp
+## Installation
+instal
+## Contribution Guidelines
+s
+## Test Instructions
+as
+## Usage
+usage
--- Develop/index.js
+++ Develop/index.js
@@ -5,38 +5,75 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        name: 'name',
-        message: 'Please enter the title of your project.',
+        name: 'questionTitle',
+        message: `Please enter the title of your project.`,
         type: 'input'
     },
     {
-        name: 'name2',
+        name: 'questionDescription',
         message: 'Please enter a detailed description of your project.',
         type: 'input'
     },
     {
-        name: 'name3',
+        name: 'questionInstallation',
         message: 'Please enter detailed installation instructions for your project.',
         type: 'input'
     },
     {
-        name: 'name4',
+        name: 'questionUsage',
+        message: 'Please enter usage information for your project.',
+        type: 'input'
+    },
+    {
+        name: 'questionContribution',
         message: 'Please enter some contribution guidelines for your project.',
         type: 'input'
     },
     {
-        name: 'name5',
+        name: 'questionTestInstructions',
         message: 'Please enter some test instructions for your project.',
         type: 'input'
     },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    console.log(answer)
+    fs.truncate(fileName, 0, (err) => { // this resets the README.md every time the function is called
+        err ? console.error(err) : console.log('README contents reset.')
+    })
+
+    if (data.questionTitle) {
+        fs.appendFile(fileName, `# ${data.questionTitle}\n`, (err) => {
+            err ? console.error(err) : console.log('Title logged!')
+        })
+    }
+    if (data.questionDescription) {
+        fs.appendFile(fileName, `## Description\n${data.questionDescription}\n`, (err) => {
+            err ? console.error(err) : console.log('Description logged!')
+        })
+    }
+    if (data.questionInstallation) {
+        fs.appendFile(fileName, `## Installation\n${data.questionInstallation}\n`, (err) => {
+            err ? console.error(err) : console.log('Installation instructions logged!')
+        })
+    }
+    if (data.questionContribution) {
+        fs.appendFile(fileName, `## Contribution Guidelines\n${data.questionContribution}\n`, (err) => {
+            err ? console.error(err) : console.log('Contribution guidelines logged!')
+        })
+    }
+    if (data.questionUsage) {
+        fs.appendFile(fileName, `## Usage\n${data.questionUsage}\n`, (err) => {
+            err ? console.error(err) : console.log('Usage information logged!')
+        })
+    }
+    if (data.questionTestInstructions) {
+        fs.appendFile(fileName, `## Test Instructions\n${data.questionTestInstructions}\n`, (err) => {
+            err ? console.error(err) : console.log('Test instructions logged!')
+        })
+    }
+
 }
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer
         .prompt(questions)

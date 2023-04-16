d29bf42423977461af481b72f129fff7545f3632
unknown <lattabun@gmail.com>
2023-04-16 12:13:33 -0400

    added async await to writeToFile function

--- Develop/README.md
+++ Develop/README.md
@@ -1,13 +1,11 @@
-# Nice title that will do fine [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html)
+# title [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
 ## Description
-An incredibly good description that will do things like make a good description or something like that such that a good description will happen and nothing else bad will happen because everybody will see such a good description teheprpr
-# Nice title that will do fine [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
-# Nice title that will do fine [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
-## Contribution Guidelines
-Contribute deez nuts on your chin or something like that!!
+description
 ## Installation
-Detailed instruction set that will do thingy thing things
+installation
 ## Usage
-Usage information that will most certainly be the most useful thing to have ever existed.
+usage
+## Contribution Guidelines
+contributions
 ## Test Instructions
-Test deez nuts hah gotteeem..!
+test
--- Develop/index.js
+++ Develop/index.js
@@ -42,109 +42,114 @@ const questions = [
     },
 ];
 
-function writeToFile(fileName, data) {
-    fs.truncate(fileName, 0, (err) => { // this resets the README.md every time the function is called
-        err ? console.error(err) : console.log('README contents reset.')
-    })
+async function writeToFile(fileName, data) {
+    try {
+        await fs.truncate(fileName, 0, (err) => { // this resets the README.md every time the function is called
+            err ? console.error(err) : console.log('README contents reset.')
+        })
 
-    if (data.questionTitle && data.questionLicense != 'None') {
-        switch (data.questionLicense) { // this switch statement contains all of the licenses that can be chosen when making a new repository
-            case 'Apache License 2.0':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'GNU General Public License v3.0':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'MIT License':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'BSD 2-Clause "Simplified" License':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'BSD 3-Clause "New" or "Revised" License':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'Boost Software License 1.0':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'Creative Commons Zero v1.0 Universal':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'Eclipse Public License 2.0':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: EPL-2.0](https://img.shields.io/badge/License-EPL%202.0-red.svg)](https://opensource.org/licenses/EPL-2.0)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'GNU Affero General Public License v3.0':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'GNU General Public License v2.0':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'GNU Lesser General Public License v2.1':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'Mozilla Public License 2.0':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
-            case 'The Unilicense':
-                fs.appendFile(fileName, `# ${data.questionTitle} [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)\n`, (err) => {
-                    err ? console.error(err) : console.log('Title and License logged!')
-                })
-                break;
+        if (data.questionTitle && data.questionLicense != 'None') {
+            switch (data.questionLicense) { // this switch statement contains all of the licenses that can be chosen when making a new repository
+                case 'Apache License 2.0':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'GNU General Public License v3.0':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'MIT License':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'BSD 2-Clause "Simplified" License':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'BSD 3-Clause "New" or "Revised" License':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'Boost Software License 1.0':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'Creative Commons Zero v1.0 Universal':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'Eclipse Public License 2.0':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: EPL-2.0](https://img.shields.io/badge/License-EPL%202.0-red.svg)](https://opensource.org/licenses/EPL-2.0)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'GNU Affero General Public License v3.0':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'GNU General Public License v2.0':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'GNU Lesser General Public License v2.1':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'Mozilla Public License 2.0':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+                case 'The Unilicense':
+                    fs.appendFile(fileName, `# ${data.questionTitle} [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)\n`, (err) => {
+                        err ? console.error(err) : console.log('Title and License logged!')
+                    })
+                    break;
+            }
+        }
+        else if (data.questionTitle) {
+            await fs.appendFile(fileName, `# ${data.questionTitle}\n`, (err) => {
+                err ? console.error(err) : console.log('Title logged!')
+            })
+        }
+        if (data.questionDescription) {
+            await fs.appendFile(fileName, `## Description\n${data.questionDescription}\n`, (err) => {
+                err ? console.error(err) : console.log('Description logged!')
+            })
+        }
+        if (data.questionInstallation) {
+            await fs.appendFile(fileName, `## Installation\n${data.questionInstallation}\n`, (err) => {
+                err ? console.error(err) : console.log('Installation instructions logged!')
+            })
+        }
+        if (data.questionUsage) {
+            await fs.appendFile(fileName, `## Usage\n${data.questionUsage}\n`, (err) => {
+                err ? console.error(err) : console.log('Usage information logged!')
+            })
+        }
+        if (data.questionContribution) {
+            await fs.appendFile(fileName, `## Contribution Guidelines\n${data.questionContribution}\n`, (err) => {
+                err ? console.error(err) : console.log('Contribution guidelines logged!')
+            })
+        }
+        if (data.questionTestInstructions) {
+            await fs.appendFile(fileName, `## Test Instructions\n${data.questionTestInstructions}\n`, (err) => {
+                err ? console.error(err) : console.log('Test instructions logged!')
+            })
         }
     }
-    else if (data.questionTitle) {
-        fs.appendFile(fileName, `# ${data.questionTitle}\n`, (err) => {
-            err ? console.error(err) : console.log('Title logged!')
-        })
-    }
-    if (data.questionDescription) {
-        fs.appendFile(fileName, `## Description\n${data.questionDescription}\n`, (err) => {
-            err ? console.error(err) : console.log('Description logged!')
-        })
-    }
-    if (data.questionInstallation) {
-        fs.appendFile(fileName, `## Installation\n${data.questionInstallation}\n`, (err) => {
-            err ? console.error(err) : console.log('Installation instructions logged!')
-        })
-    }
-    if (data.questionContribution) {
-        fs.appendFile(fileName, `## Contribution Guidelines\n${data.questionContribution}\n`, (err) => {
-            err ? console.error(err) : console.log('Contribution guidelines logged!')
-        })
-    }
-    if (data.questionUsage) {
-        fs.appendFile(fileName, `## Usage\n${data.questionUsage}\n`, (err) => {
-            err ? console.error(err) : console.log('Usage information logged!')
-        })
-    }
-    if (data.questionTestInstructions) {
-        fs.appendFile(fileName, `## Test Instructions\n${data.questionTestInstructions}\n`, (err) => {
-            err ? console.error(err) : console.log('Test instructions logged!')
-        })
+    catch (err) {
+        console.error(err);
     }
 }
 
